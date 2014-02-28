e6d66eef71e0ebcb25e778f611309f7187ff42bb
AlexanderBilz <alexander-bilz@t-online.de>
2014-02-28 18:32:44 +0100

    Startup Script
    Added script for automatically starting the stream

---
+++ Startup/startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -----------------------------------------------
+echo 
+echo               Raspberry Pi Tank
+echo 
+echo -----------------------------------------------
+rm -rf /tmp/stream
+#Remotve Directory before creating new one!
+echo Creating directory
+mkdir /tmp/stream
+raspistill --nopreview -w 640 -h 480 -q 5 -o /tmp/stream/pic.jpg -tl 100 -t 9999999 -th 0:0:0 &
+#Start the service
+LD_LIBRARY_PATH=/usr/local/lib mjpg_streamer -i "input_file.so -f /tmp/stream -n pic.jpg" -o "output_http.so -w /usr/local/www"&
+
+echo 
+echo --------------Finished successfull-------------
