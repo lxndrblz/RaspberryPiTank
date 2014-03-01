ec98767ba32d38097cf842d1d5820900b47bb6a1
AlexanderBilz <alexander-bilz@t-online.de>
2014-03-01 12:52:18 +0100

    Update on the lifestream
    Automatically start the Cam

--- Startup/AutoCam.sh
+++ Startup/AutoCam.sh
@@ -11,7 +11,7 @@ echo Creating directory
 mkdir /tmp/stream
 raspistill --nopreview -w 640 -h 480 -q 5 -o /tmp/stream/pic.jpg -tl 100 -t 9999999 -th 0:0:0 &
 #Start the service
-LD_LIBRARY_PATH=/usr/local/lib mjpg_streamer -i "input_file.so -f /tmp/stream -n pic.jpg" -o "output_http.so -w /usr/local/www"&
+LD_LIBRARY_PATH=/usr/local/lib mjpg_streamer -i "input_file.so -f /tmp/stream -n pic.jpg" -o "output_http.so -w /usr/local/www"
 
 echo 
 echo --------------Finished successfull-------------
